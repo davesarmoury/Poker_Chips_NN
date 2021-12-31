5d1f0d48e27bc95cacbf827fb97eb8fbcf6f4143
Dave Niewinski <davesarmoury@gmail.com>
2021-12-31 09:48:23 -0500

    Moved model

--- verify.sh
+++ verify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd yolov5
-python3 detect.py --weights best.pt --img 416 --conf 0.5 --source ../test/images
+python3 detect.py --weights ../model/best.pt --img 416 --conf 0.5 --source ../test/images
