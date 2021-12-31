0c39af75a9daeaddd0eee727d351228e058b69bc
Dave Niewinski <davesarmoury@gmail.com>
2021-12-31 09:17:32 -0500

    Initiial training

--- verify.sh
+++ verify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd yolov5
-python3 detect.py --weights runs/train/exp/weights/best.pt --img 576 --conf 0.5 --source ../test/images
+python3 detect.py --weights best.pt --img 416 --conf 0.5 --source ../test/images
